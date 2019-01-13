b4ba7e73e92f6b1452c035ca499c9fcf9178d25b
Ryzhehvost <kotlyar.andrey@gmail.com>
2019-01-13 18:21:20 +0200

    Initial implementation

---
+++ Ryzhehvost.CaseInsensitiveASF/CaseInsensitiveASF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Composition;
+using ArchiSteamFarm;
+using ArchiSteamFarm.Plugins;
+
+namespace Ryzhehvost.CaseInsensitiveASF {
+	[Export(typeof(IPlugin))]
+	public sealed class CaseInsensitiveASF : IBotsComparer {
+		public string Name => nameof(CaseInsensitiveASF);
+		public Version Version => typeof(CaseInsensitiveASF).Assembly.GetName().Version;
+
+		public void OnLoaded() {
+			ASF.ArchiLogger.LogGenericInfo("Case Insensitive ASF Plugin by Ryzhehvost, powered by ginger cats");
+		}
+
+		public StringComparer BotsComparer => StringComparer.InvariantCultureIgnoreCase;
+
+	}
+}
