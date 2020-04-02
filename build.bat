9a2b2bc643de857cca993e0c5107440d9ae6c1e1
Ryzhehvost <kotlyar.andrey@gmail.com>
2020-04-02 22:11:37 +0300

    update build script

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+if not exist ArchiSteamFarm\ArchiSteamFarm (git submodule update --init)
+git submodule foreach "git fetch origin; git checkout $(git rev-list --tags --max-count=1);"
 del .\Ryzhehvost.CaseInsensitiveASF\*.zip
 dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\Ryzhehvost.CaseInsensitiveASF\CaseInsensitiveASF.zip CaseInsensitiveASF-netf.zip 
-dotnet publish -c "Release" -f "netcoreapp3.0" -o "out/generic" "/p:LinkDuringPublish=false"
+dotnet publish -c "Release" -f "netcoreapp3.1" -o "out/generic" "/p:LinkDuringPublish=false"
