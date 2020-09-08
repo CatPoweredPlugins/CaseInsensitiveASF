4c207338184547d9d3d08280df01683c0353c7c7
Ryzhehvost <kotlyar.andrey@gmail.com>
2020-09-08 15:15:47 +0300

    implement support for nullable reference variables

--- CaseInsensitiveASF/CaseInsensitiveASF.cs
+++ CaseInsensitiveASF/CaseInsensitiveASF.cs
@@ -7,7 +7,7 @@ namespace Ryzhehvost.CaseInsensitiveASF {
 	[Export(typeof(IPlugin))]
 	public sealed class CaseInsensitiveASF : IBotsComparer {
 		public string Name => nameof(CaseInsensitiveASF);
-		public Version Version => typeof(CaseInsensitiveASF).Assembly.GetName().Version;
+		public Version Version => typeof(CaseInsensitiveASF).Assembly.GetName().Version ?? new Version("0");
 
 		public void OnLoaded() {
 			ASF.ArchiLogger.LogGenericInfo("Case Insensitive ASF Plugin by Ryzhehvost, powered by ginger cats");
