874992d41dd7cd9b7619bf8e4d02bbf4dc0c7771
Rudokhvist <kotlyar.andrey@gmail.com>
2024-09-17 00:11:15 +0300

    switch to organization, enable auto-updates

--- CaseInsensitiveASF/CaseInsensitiveASF.cs
+++ CaseInsensitiveASF/CaseInsensitiveASF.cs
@@ -1,21 +1,75 @@
 using System;
 using System.Composition;
+using System.Globalization;
 using System.Threading.Tasks;
 using ArchiSteamFarm.Core;
+using ArchiSteamFarm.Localization;
 using ArchiSteamFarm.Plugins.Interfaces;
+using ArchiSteamFarm.Web.GitHub;
+using ArchiSteamFarm.Web.GitHub.Data;
 
-namespace Rudokhvist.CaseInsensitiveASF {
-	[Export(typeof(IPlugin))]
-	public sealed class CaseInsensitiveASF : IBotsComparer {
-		public string Name => nameof(CaseInsensitiveASF);
-		public Version Version => typeof(CaseInsensitiveASF).Assembly.GetName().Version ?? new Version("0.0.0.0");
+namespace CaseInsensitiveASF;
+[Export(typeof(IPlugin))]
+internal sealed class CaseInsensitiveASF : IBotsComparer, IGitHubPluginUpdates {
+	public string Name => nameof(CaseInsensitiveASF);
+	public Version Version => typeof(CaseInsensitiveASF).Assembly.GetName().Version ?? new Version("0.0.0.0");
 
-		public Task OnLoaded() {
-			ASF.ArchiLogger.LogGenericInfo("Case Insensitive ASF Plugin by Rudokhvist, powered by ginger cats");
-			return Task.CompletedTask;
+	public Task OnLoaded() {
+		ASF.ArchiLogger.LogGenericInfo("Case Insensitive ASF Plugin by Rudokhvist, powered by ginger cats");
+		return Task.CompletedTask;
+	}
+
+	public StringComparer BotsComparer => StringComparer.OrdinalIgnoreCase;
+
+	public string RepositoryName => "CatPoweredPlugins/CaseInsensitiveASF";
+
+	public async Task<Uri?> GetTargetReleaseURL(Version asfVersion, string asfVariant, bool asfUpdate, bool stable, bool forced) {
+		ArgumentNullException.ThrowIfNull(asfVersion);
+		ArgumentException.ThrowIfNullOrEmpty(asfVariant);
+
+		if (string.IsNullOrEmpty(RepositoryName)) {
+			ASF.ArchiLogger.LogGenericError(string.Format(CultureInfo.CurrentCulture, Strings.WarningFailedWithError, nameof(RepositoryName)));
+
+			return null;
 		}
 
-		public StringComparer BotsComparer => StringComparer.OrdinalIgnoreCase;
+		ReleaseResponse? releaseResponse = await GitHubService.GetLatestRelease(RepositoryName, stable).ConfigureAwait(false);
 
+		if (releaseResponse == null) {
+			return null;
+		}
+
+		Version newVersion = new(releaseResponse.Tag);
+
+		if (!(Version.Major == newVersion.Major && Version.Minor == newVersion.Minor && Version.Build == newVersion.Build) && !(asfUpdate || forced)) {
+			ASF.ArchiLogger.LogGenericInfo(string.Format(CultureInfo.CurrentCulture, "New {0} plugin version {1} is only compatible with latest ASF version", Name, newVersion));
+			return null;
+		}
+
+
+		if (Version >= newVersion & !forced) {
+			ASF.ArchiLogger.LogGenericInfo(string.Format(CultureInfo.CurrentCulture, Strings.PluginUpdateNotFound, Name, Version, newVersion));
+
+			return null;
+		}
+
+		if (releaseResponse.Assets.Count == 0) {
+			ASF.ArchiLogger.LogGenericWarning(string.Format(CultureInfo.CurrentCulture, Strings.PluginUpdateNoAssetFound, Name, Version, newVersion));
+
+			return null;
+		}
+
+		ReleaseAsset? asset = await ((IGitHubPluginUpdates) this).GetTargetReleaseAsset(asfVersion, asfVariant, newVersion, releaseResponse.Assets).ConfigureAwait(false);
+
+		if ((asset == null) || !releaseResponse.Assets.Contains(asset)) {
+			ASF.ArchiLogger.LogGenericWarning(string.Format(CultureInfo.CurrentCulture, Strings.PluginUpdateNoAssetFound, Name, Version, newVersion));
+
+			return null;
+		}
+
+		ASF.ArchiLogger.LogGenericInfo(string.Format(CultureInfo.CurrentCulture, Strings.PluginUpdateFound, Name, Version, newVersion));
+
+		return asset.DownloadURL;
 	}
+
 }
